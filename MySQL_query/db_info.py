676eb8f0fc1441ea7581026e38e94d2ea9ac04ef
JunGyuRyu <qurrpdnjs1@naver.com>
2024-04-03 13:48:13 +0900

    Chore: add db_info.py to .gitignore

--- MySQL_query/db_info.py
+++
@@ -1,5 +0,0 @@
-def sql_info():
-    db_url = "mysql+pymysql://user43:user43@0.tcp.jp.ngrok.io:14526/team4"
-    # csv_path = "C:/Users/JunGyuRyu/Desktop/FINAL_PROJECT/데이터/사용할 데이터/유사도 구한 데이터/similarity.csv"
-    csv_path = "C:/Users/JunGyuRyu/Desktop/FINAL_PROJECT/데이터/사용할 데이터/유사도 상위 5개 데이터/top_5_card_recommendations.csv"
-    return db_url, csv_path
