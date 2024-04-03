2ca74d0fdf0e726a342557cefa997f83f463de8b
Ryunos96 <sinple010@gmail.com>
2024-04-03 14:37:40 +0900

    Feat: RequestMapping 추가

--- bespoke/src/main/java/FINAL/bespoke/controller/CardDesignController.java
+++
@@ -1,5 +0,0 @@
-package FINAL.bespoke.controller;
-
-public class CardDesignController {
-	// cardesign 담당 김준우 당첨!!!
-}
--- bespoke/src/main/java/FINAL/bespoke/controller/ImageController.java
+++ bespoke/src/main/java/FINAL/bespoke/controller/ImageController.java
@@ -5,14 +5,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/aiimage")
 public class ImageController {
 
     @Autowired
     private ImageService imageService;
 
-    @GetMapping("/aiimage/carddesign")
+    @GetMapping("/carddesign")
     public String showHomePage(Model model) {
         // 이미지 생성 로직을 통해 ImageDto 객체를 생성합니다.
  //   	ImageDto aiImage;
