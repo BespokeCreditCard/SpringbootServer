c2afac54618f2fea5df5326f89e2b7affaf23831
Ryunos96 <sinple010@gmail.com>
2024-04-04 16:15:37 +0900

    Feat: Elastic Springboot 연동 및 데이터 가져오는 기능 추가

--- bespoke/src/main/java/FINAL/bespoke/service/ElasticService.java
+++ bespoke/src/main/java/FINAL/bespoke/service/ElasticService.java
@@ -1,8 +1,13 @@
 package FINAL.bespoke.service;
 
+import java.io.IOException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import co.elastic.clients.elasticsearch.ElasticsearchClient;
+import co.elastic.clients.elasticsearch._types.ElasticsearchException;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import co.elastic.clients.elasticsearch.core.GetResponse;
 
@@ -15,12 +20,26 @@ public class ElasticService {
         this.esClient = esClient;
     }
 	
-    public void fetchProduct() {
-        GetResponse<ObjectNode> response = esClient.get(g -> g
-            .index("products")
-            .id("bk-1"),
-            ObjectNode.class     
-        );
+    public GetResponse<ObjectNode> fetchData() {
+    	GetResponse<ObjectNode> response = null;
+        try {
+			response = esClient.get(g -> g
+			    .index("result_bulk")
+			    .id("15"),
+			    ObjectNode.class     
+			);
+//	        System.out.println("#####################################################################");
+//			System.out.println(response.toString());
+//			System.out.println("#####################################################################");
+		} catch (ElasticsearchException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        return response;
+//        return ObjectNode.class;
         
     }
 }
