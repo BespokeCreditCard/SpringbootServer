045d1264ed04fbac9835349cc1929f49a40056ff
Ryunos96 <sinple010@gmail.com>
2024-04-12 18:38:25 +0900

    Feat: 카드 한눈에보기 페이지 5개 받아와서 출력하는 기능 구현

--- bespoke/src/main/java/FINAL/bespoke/service/ElasticService.java
+++ bespoke/src/main/java/FINAL/bespoke/service/ElasticService.java
@@ -1,12 +1,16 @@
 package FINAL.bespoke.service;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import co.elastic.clients.elasticsearch.ElasticsearchClient;
 import co.elastic.clients.elasticsearch._types.ElasticsearchException;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import co.elastic.clients.elasticsearch.core.GetResponse;
+import co.elastic.clients.elasticsearch.core.SearchResponse;
 
 @Service
 public class ElasticService {
@@ -17,26 +21,48 @@ public class ElasticService {
         this.esClient = esClient;
     }
 	
-    public GetResponse<ObjectNode> fetchData() {
-    	GetResponse<ObjectNode> response = null;
-        try {
-			response = esClient.get(g -> g
-			    .index("result_bulk")
-			    .id("6"),
-			    ObjectNode.class     
-			);
-//	        System.out.println("#####################################################################");
-//			System.out.println(response.toString());
-//			System.out.println("#####################################################################");
-		} catch (ElasticsearchException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-        return response;
-//        return ObjectNode.class;
+    // test elasticsearch for 문
+    public List<GetResponse<ObjectNode>> fetchData(List<Integer> imageId) {
+    	List<GetResponse<ObjectNode>> responses = new ArrayList<>();
+    	for (Integer imageIdtmp : imageId) {
+	        try {
+	        	GetResponse<ObjectNode> response = esClient.get(g -> g
+				    .index("result_bulk")
+				    .id(imageIdtmp.toString()), ObjectNode.class);
+	        	responses.add(response);
+			} catch (ElasticsearchException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+    	}
+        return responses;
         
     }
+    
+    
+//    public GetResponse<ObjectNode> fetchData(List<Integer> imageId) {
+//    	GetResponse<ObjectNode> response = null;
+//        try {
+////        	response = esClient.search(g -> g
+////    			    .index("result_bulk")
+////    			    .query(q -> q
+////    			    		.match(t -> t
+////    			    				.field("id")
+////    			    				.query(imageId))),
+////    			    ObjectNode.class     
+////    			);
+//			response = esClient.get(g -> g
+//			    .index("result_bulk")
+//			    .id("6"),
+//			    ObjectNode.class     
+//			);
+//		} catch (ElasticsearchException e) {
+//			e.printStackTrace();
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//		}
+//        return response;
+//        
+//    }
 }
