715f2f54fb603117a0f315be493c2ada6758f4ff
JunGyuRyu <qurrpdnjs1@naver.com>
2024-04-15 10:57:21 +0900

    Refactor: b64 형식 불러오는 코드 수정 및 dall-e 테스트용 코드 추가

--- bespoke/src/main/java/FINAL/bespoke/service/AiGeneratorService.java
+++ bespoke/src/main/java/FINAL/bespoke/service/AiGeneratorService.java
@@ -16,9 +16,9 @@ public class AiGeneratorService {
     @Resource(name = "getOpenAiService")
     private final OpenAiService openAiService;
 
-    public String generatePicture(String prompt) {
+    public String generatePrompt(String prompt) {
     	try {
-	        CreateImageRequest createImageRequest = CreateImageRequest.builder()
+    		CreateImageRequest createImageRequest = CreateImageRequest.builder()
 	        		.model("dall-e-3")
 	        		.quality("hd")
                     .prompt(prompt)
@@ -32,6 +32,36 @@ public class AiGeneratorService {
 //	        String url = openAiService.createImage(createImageRequest).getData().get(0).getUrl();
 //	        return url;
 	        //이미지 URL 대신 base64 data를 리턴하는 것으로 변경!
+	        String b64 = openAiService.createImage(createImageRequest).getData().get(0).getB64Json();
+	        return b64;
+    	} catch (Exception e) {
+            // 이미지 생성 중 에러가 발생하면 예외를 처리합니다.
+            e.printStackTrace();
+            return null;
+        }
+    }
+    
+//    public String generateImage(String prompt, String imgPath) {
+	public String generateImage(String prompt) {
+    	try {
+    		CreateImageRequest createImageRequest = CreateImageRequest.builder()
+//    		CreateImageVariationRequest createImageVariationRequest = CreateImageVariationRequest.builder()
+//	        		.model("dall-e-3")
+	        		.model("dall-e-3")
+	        		.quality("hd")
+                    .prompt(prompt)
+                    .style("vivid")
+//                    .size("1024x1024")
+                    .size("1024x1792")
+                    .n(1)
+//                    .responseFormat("url")
+	                .responseFormat("b64_json") //b64_json 포맷으로의 반환을 위해 이 코드 추가!
+                    .build();
+//	        String url = openAiService.createImage(createImageRequest).getData().get(0).getUrl();
+//	        return url;
+	        //이미지 URL 대신 base64 data를 리턴하는 것으로 변경!
+	        
+//	        String b64 = openAiService.createImageVariation(createImageVariationRequest, imgPath).getData().get(0).getB64Json();
 	        String b64 = openAiService.createImage(createImageRequest).getData().get(0).getB64Json();
 	        return b64;
     	} catch (Exception e) {
