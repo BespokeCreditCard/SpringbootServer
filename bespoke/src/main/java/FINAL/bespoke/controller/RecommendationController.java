667530b7cedb7c241fbadd7cea13144f31f1ccd0
Ryunos96 <sinple010@gmail.com>
2024-04-16 17:05:27 +0900

    Feat: 캐러셀에 카드 이미지 연결 및 elasticsearch 데이터 연결

--- bespoke/src/main/java/FINAL/bespoke/controller/RecommendationController.java
+++ bespoke/src/main/java/FINAL/bespoke/controller/RecommendationController.java
@@ -1,12 +1,23 @@
 package FINAL.bespoke.controller;
 
-import FINAL.bespoke.service.GetUrlService;
 import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+
+import FINAL.bespoke.model.entity.User;
+import FINAL.bespoke.service.ElasticService;
+import FINAL.bespoke.service.GetUrlService;
+import FINAL.bespoke.service.ReceiveCardService;
+import co.elastic.clients.elasticsearch.core.GetResponse;
+import jakarta.servlet.http.HttpServletRequest;
 
 @Controller
 @RequestMapping("/recommendation_view")
@@ -14,23 +25,43 @@ public class RecommendationController {
 
     private final GetUrlService recommendationService;
 
+    private final ElasticService elasticService;
+    
+    private final ReceiveCardService receiveCardService;
+    
     @Autowired
-    public RecommendationController(GetUrlService recommendationService) {
+    public RecommendationController(GetUrlService recommendationService, ElasticService elasticService, ReceiveCardService receiveCardService) {
         this.recommendationService = recommendationService;
+        this.elasticService = elasticService;
+        this.receiveCardService = receiveCardService;
     }
         
     @GetMapping("/recommendation")
-    public String showRecommendation(Model model) {
+    public String showRecommendation(Model model, HttpServletRequest request) throws JsonProcessingException {
         // RecommendationService를 통해 recommendation 테이블의 데이터를 가져옴
 //        List<Integer> imageList = recommendationService.getRecommendation();
 //
 //        List<String> imageUrls = recommendationService.getImageUrls(imageList);
         
         // recommendationDTO를 모델에 추가하여 JSP 페이지로 전달
-//        model.addAttribute("imageUrls", imageUrls);
+        model.addAttribute("imageUrls", imageUrls);
 
+        User user = receiveCardService.findUserId(request);
+        List<GetResponse<ObjectNode>> response = elasticService.fetchData(imageList);
+        
+        List<List<String>> categoryClass = elasticService.ElasticSearchJsonToTextClassInCategory(response);
+        
+        ObjectMapper objectMapper = new ObjectMapper();
+        System.out.println(categoryClass);
+        String categoryClassJson = objectMapper.writeValueAsString(categoryClass);
+        System.out.println(categoryClassJson);
+        model.addAttribute("categoryClassJson", categoryClassJson);
+        
+        
+//        model.addAttribute("categoryClass", categoryClass);
+        
     	System.out.println("################################");
-//    	System.out.println(imageUrls);
+    	System.out.println("categoryClass: "+ categoryClass);
     	System.out.println("################################");
         // recommendation.jsp로 이동
         return "recommendation_view/recommendation";
