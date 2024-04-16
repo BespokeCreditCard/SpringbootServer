30f5f2ea90647b15c4b0a1251f1fba7614ef5009
JunGyuRyu <qurrpdnjs1@naver.com>
2024-04-16 12:02:52 +0900

    Refactor: sql 파일 수정

--- MySQL_query/final_project.sql
+++ MySQL_query/final_project.sql
@@ -2,6 +2,6 @@ use team4;
 select * from consumption;
 select * from image;
 select * from image_entity;
-SELECT * FROM top5_recommendation;
-SELECT COUNT(*) AS '전체 행의 개수' FROM top5_recommendation;
+SELECT * FROM recommendation;
+SELECT COUNT(*) AS '전체 행의 개수' FROM recommendation;
 SELECT COUNT(*) AS '전체 행의 개수' FROM similarity;
