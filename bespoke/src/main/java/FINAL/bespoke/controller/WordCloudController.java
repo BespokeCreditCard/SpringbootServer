556df45e91884752c5e048590b5cf190c0132d3c
ioshe <sdcard11291@gmail.com>
2024-04-19 11:46:52 +0900

    Feat : wordcloud 백 프론트 연결

--- bespoke/src/main/java/FINAL/bespoke/controller/WordCloudController.java
+++ bespoke/src/main/java/FINAL/bespoke/controller/WordCloudController.java
@@ -1,8 +1,33 @@
 package FINAL.bespoke.controller;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.fasterxml.jackson.databind.node.ObjectNode;
+
+import FINAL.bespoke.service.ElasticService;
+import co.elastic.clients.elasticsearch.core.GetResponse;
 
 @Controller
 public class WordCloudController {
-
+	private final ElasticService elasticService;
+	
+	public WordCloudController(ElasticService elasticService) {
+		this.elasticService = elasticService;
+	}
+	
+	@GetMapping("wordcloud")
+	public String goWordCloud(Model model) {
+		
+		int cardid = 1;
+		GetResponse<ObjectNode> response = elasticService.fetchCardidData(String.valueOf(cardid));
+		List<String> cardWordDetail = elasticService.ElasticSearchJsonTocardWordData(response);
+		model.addAttribute("wordcloud",cardWordDetail);
+		System.out.println(cardWordDetail); 
+		return "wordcloud";
+	}
+	
 }
