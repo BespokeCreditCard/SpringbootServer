bb78eb5fe6b624473961221da591f5b183e98607
ioshe <sdcard11291@gmail.com>
2024-04-22 11:48:18 +0900

    Feat : 버튼을 눌렀을 때 바뀌게 하기 위해 여러값을 받아서 modelAddattribute

--- bespoke/src/main/java/FINAL/bespoke/controller/WordCloudController.java
+++ bespoke/src/main/java/FINAL/bespoke/controller/WordCloudController.java
@@ -10,6 +10,7 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 
+import FINAL.bespoke.model.dto.wordDetailsDTO;
 import FINAL.bespoke.service.ElasticService;
 import co.elastic.clients.elasticsearch.core.GetResponse;
 import jakarta.servlet.http.HttpServletRequest;
@@ -31,17 +32,21 @@ public class WordCloudController {
 		// 세션에서 값 불러오기
 		List<Integer> cardId = List.of(22, 6, 1, 2, 3);
 		List<GetResponse<ObjectNode>> response = elasticService.fetchDataElastic(cardId,"card_word");
-		List<List<String>> cardWordDetail = elasticService.ElasticSearchJsonTocardWordData(response);
+		wordDetailsDTO wordDetailsDTO = elasticService.ElasticSearchJsonTocardWordData(response);
 //		model.addAttribute("wordcloud",cardWordDetail);
-//		System.out.println(cardWordDetail); 
+//		System.out.println(cardWordDetail);
+		System.out.println(wordDetailsDTO.getCardWord());
 		String json = "";
-        try {
-            json = objectMapper.writeValueAsString(cardWordDetail);
-        } catch (JsonProcessingException e) {
-            e.printStackTrace();
-        }
-        model.addAttribute("wordcloudJson", json.replace("\"", "\\\""));
-        System.out.println("json="+json);
+		try {
+			json = objectMapper.writeValueAsString(wordDetailsDTO.getCardWord());
+		} catch (JsonProcessingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        model.addAttribute("wordcloudJson",json);
+        model.addAttribute("word",wordDetailsDTO.getCardWord());
+        System.out.println(json);
+        model.addAttribute("worddetailscardname",wordDetailsDTO.getCardName());
 		return "wordcloud";
 	}
 	
